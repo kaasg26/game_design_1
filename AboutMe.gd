dfc01283c3e1b29fe268808e5aedee65ee9f5236
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-04 14:23:57 -0500

    Update AboutMe.gd
    update 1

--- AboutMe.gd
+++ AboutMe.gd
@@ -6,12 +6,10 @@ extends Control
 
 func _on_button_pressed() -> void:
 	$Label.text = "I Own 2 Swords"
-	
-	
+
 
 func _on_button_2_pressed() -> void:
 	$Label.text = ""
-
 
 func _on_button_3_pressed() -> void:
 	get_tree().quit()

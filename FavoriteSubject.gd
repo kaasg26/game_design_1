d5105be0e45f7949b5a3b4889d85cfc05deb0b57
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-05 13:54:52 -0500

    9/5

--- FavoriteSubject.gd
+++ FavoriteSubject.gd
@@ -1,1 +1,16 @@
 extends Control
+
+
+
+func _on_button_pressed() -> void:
+	$Label.text = "Anything to do with Art or Tech"
+
+
+
+func _on_button_2_pressed() -> void:
+	$Label.text = ""
+
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()

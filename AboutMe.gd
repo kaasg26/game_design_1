5fbfc257e2b67ab785966bd6993ea7e1f3ad1c98
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-05 14:33:53 -0500

    9/5

--- AboutMe.gd
+++ AboutMe.gd
@@ -5,7 +5,7 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	$Label.text = "I Own 2 Swords"
+	$Label.text = "I Own 2 Swords and have them Hanging on my wall."
 
 
 func _on_button_2_pressed() -> void:

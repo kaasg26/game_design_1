6b264c6462e5e34400b83af841f2fc9a32d6b85c
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-06 14:15:29 -0500

    9/6

--- prog_54b.gd
+++ prog_54b.gd
@@ -11,15 +11,15 @@ func _on_button_pressed() -> void:
 	var three = int($txt3.text)
 	var four = int($txt4.text)
 	var sum = one + two + three + four
-	var average = one + two + three + four / 4
+	var average = sum / 4
 	$lblSum.text = "Sum: " + str(sum)
 	$lblAve.text = "Average: " + str(average)
 
 func _on_button_2_pressed() -> void:
 	$txt1.text = ""
-	$txt1.text = ""
-	$txt1.text = ""
-	$txt1.text = ""
+	$txt2.text = ""
+	$txt3.text = ""
+	$txt4.text = ""
 	$lblSum.text = "Sum: "
 	$lblAve.text = "Average: "
 

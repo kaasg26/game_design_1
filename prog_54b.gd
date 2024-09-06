281d59961d432654faaf662542299a752e0b5f9a
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-06 14:10:54 -0500

    9/6

--- prog_54b.gd
+++ prog_54b.gd
@@ -1,1 +1,28 @@
 extends Control
+
+
+
+
+
+func _on_button_pressed() -> void:
+
+	var one = int($txt1.text)
+	var two = int($txt2.text)
+	var three = int($txt3.text)
+	var four = int($txt4.text)
+	var sum = one + two + three + four
+	var average = one + two + three + four / 4
+	$lblSum.text = "Sum: " + str(sum)
+	$lblAve.text = "Average: " + str(average)
+
+func _on_button_2_pressed() -> void:
+	$txt1.text = ""
+	$txt1.text = ""
+	$txt1.text = ""
+	$txt1.text = ""
+	$lblSum.text = "Sum: "
+	$lblAve.text = "Average: "
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()

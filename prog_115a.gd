73d9e976628f59da55cb38c6e0c699204a71c558
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-18 14:10:25 -0500

    9/18

---
+++ prog_115a.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+func _on_button_pressed() -> void:
+	$ItemList.add_item("Number")
+	for num in range (2,36+1, 2):
+		var line = str(num)
+		$ItemList.add_item(line)
+#range(stop)
+#range(start,stop)
+#range(start,stop,step)
+
+func _on_button_2_pressed() -> void:
+	$ItemList.clear()
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()

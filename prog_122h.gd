d7c6421bdfe740e91900ff37ce3d1d0cdbb1fdf1
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-09-23 14:35:07 -0500

    9/23

--- prog_122h.gd
+++ prog_122h.gd
@@ -1,1 +1,21 @@
 extends Control
+
+
+func _on_button_pressed() -> void:
+	$ItemList.add_item("x    x^2    sqrt(x)    x^3    sqrt(x, 1/4)    ")
+	for num in range(1, 21):
+		
+		var Sqr2 = num ** 2
+		var SqT2 = sqrt(num)
+		var Sqr3 = num ** 3
+		var SqI4 = num ** 0.25
+		var line = "%d    %d    %d    %d    %d" % [num, Sqr2, SqT2, Sqr3. SqI4]
+		$ItemList.add_item(line)
+
+
+func _on_button_2_pressed() -> void:
+	$ItemList.clear()
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()

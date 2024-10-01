e411b1e209457d2c72dc5536d829feb5cda60bd7
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-10-01 14:36:15 -0500

    10/1

--- lp_4_3.gd
+++ lp_4_3.gd
@@ -7,6 +7,11 @@ func _on_button_pressed() -> void:
 	var Eggs = int($txtDoz.text)
 	var Dozens = floor(Eggs / 12)
 	var remainder = Eggs % 12
+	var price 
+	if Eggs <= 3:
+		price = Dozens * 0.50 + remainder * 0.50
+	elif Eggs <= 5:
+		price = Dozens * 0.45 + remainder * 0.45
 
 
 func _on_button_2_pressed() -> void:

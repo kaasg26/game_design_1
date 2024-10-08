c5aabfe62e703e0737eb5b9abf94db9c2b78d7ed
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-10-08 14:36:30 -0500

    10/8

--- calculator_w__func's.gd
+++ calculator_w__func's.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-var num1 = int(LineEdit.text)
-var
+var num1 = int($LineEdit.text)
+var num2 = int($LineEdit2.text)
 
 func myAdd(num1, num2):
-	
+	num1 + num2
 
 func mySub(num1, num2):
+	num1 - num2
 
 func myMul(num1, num2):
 
@@ -15,3 +16,9 @@ func myDiv(num1, num2):
 func myPow(num1, num2):
 
 func myMod(num1, num2):
+
+func _on_btn_mul_pressed():
+	var n1 = int($txtNum1.text)
+	var n2 = int($txtNum2.text)
+	var result = myMul(n1, n2)
+	$lblout.text = str(result)

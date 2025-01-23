67f040bab1c3bc9caaff6c9cea757d6d7e3f1e32
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2025-01-23 11:54:03 -0600

    1/23

--- Games/Marrow/mar_enemy.gd
+++ Games/Marrow/mar_enemy.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+@onready var _focus = $Focus
+@onready var progress_bar = $ProgressBar
+@onready var animation_player = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var MAX_HEALTH: float = 10
 
+var health: float = 10:
+	set(value):
+		health = value
+		_update_progress_bar()
+		_play_animation()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _update_progress_bar():
+	progress_bar.value = (health/MAX_HEALTH) * 100
+
+func _play_animation():
+	animation_player.play("hurt")
+
+func focus():
+	_focus.show()
+
+func unfocus():
+	_focus.hide()
+	
--- Games/Marrow/mar_enemy_group.gd
+++ Games/Marrow/mar_enemy_group.gd
@@ -1,8 +1,25 @@
 extends Node2D
 
 var enemies: Array = []
+var index: int = 0
 
 func _ready():
 	enemies = get_children()
 	for i in enemies.size():
 		enemies[i].position = Vector2(i*120, i*80)
+	
+	enemies[0].focus()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_up"):
+		if index > 0:
+			index -= 1
+			switch_focus(index, index + 1)
+	if Input.is_action_just_pressed("ui_down"):
+		if index < enemies.size() - 1:
+			index =+ 1
+			switch_focus(index, index - 1)
+
+func switch_focus(x,y):
+	enemies[x].focus()
+	enemies[y].focus()
